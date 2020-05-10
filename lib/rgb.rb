0ed5cb0dd761e302348f3c168dfc4ed82cc31309
kaifumi <kaifumi1001@gmail.com>
2020-05-10 15:06:12 +0900

    add_lesson_1

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -3,3 +3,11 @@ def to_hex(r, g, b)
     hex + n.to_s(16).rjust(2, '0')
   end
 end
+
+# 正規表現とscanメソッドを使うとこうなる
+# scanメソッドは正規表現にマッチした文字列を配列にして返す
+'#12abcd'.scan(/\w\w/) #=> ["12", "ab", "cd"]
+
+def to_ints(hex)
+  hex.scan(/\w\w/).map(&:hex)
+end
